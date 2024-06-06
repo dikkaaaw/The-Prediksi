0e9aa3cc6ea373dfb88a54f62eaa6b9bb0862c93
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-06 21:08:40 +0700

    add homepage

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Prediksi Motor Club</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link rel="stylesheet" href="./assets/css/index.css" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+    rel="stylesheet" />
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid d-flex justify-content-between">
+      <div>
+        <img src="./assets/image/logo_prediksi.jpg" class="img-fluid rounded-5" alt="" />
+      </div>
+      <div>
+        <a class="navbar-brand" href="index.php"><strong>PREDIKSI CLUB</strong></a>
+      </div>
+      <div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
+          aria-controls="offcanvasExample">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample"
+          aria-labelledby="offcanvasExampleLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasExampleLabel">
+              <strong>THE PREDIKSI CLUB</strong>
+            </h5>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="index.php
+                  ">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="profile.php
+                  ">Profile</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="about_us.php">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="product.php">Merchandise</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container-fluid text-center mt-5 mb-4">
+    <h2>Welcome to the Prediksi Club</h2>
+    <p>
+      The Prediksi adalah klub sepeda motor yang digagas oleh Andre Taulany
+      dan Ronal Surapradja sejak tahun 2018, beranggotakan selebritas dan
+      figur publik Indonesia sebagai sarana hiburan semata dan ajang
+      silaturahmi. Klub ini terdiri dari anggota dengan berbagai profesi,
+      namun didominasi oleh komedian. Meski awalnya digagas sebagai klub
+      sepeda motor, klub ini kemudian merambah bidang-bidang lain seperti
+      olahraga dan musik.
+    </p>
+    <div class="row mt-3">
+      <div class="col-md-4">
+        <div class="image-container">
+          <img
+            src="https://cdn1-production-images-kly.akamaized.net/IXOlcsuKm09c59s3Nf4U-16elAU=/1200x1200/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/4356881/original/089660900_1678713660-theprediksi_-13-03-2023-0003.jpg"
+            alt="" />
+          <div class="image-text">PREDIKSI CLUB</div>
+        </div>
+        <div class="image-container mt-4">
+          <img
+            src="https://asset.kompas.com/crops/xrSzVaIxxE_7KHzhBzJUim2fZro=/74x176:586x517/750x500/data/photo/2022/09/05/63160bc299e61.jpg"
+            alt="" />
+          <div class="image-text">PREDIKSI CLUB</div>
+        </div>
+        <div class="image-container mt-4">
+          <img src="https://cdn.antaranews.com/cache/1200x800/2022/09/28/WhatsApp-Image-2022-09-28-at-19.24.23.jpeg"
+            alt="" />
+          <div class="image-text">PREDIKSI CLUB</div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="image-container">
+          <img
+            src="https://studio.mrngroup.co/storage/app/media/Prambors/Editorial%202/THE%20PREDIKSI-20220915195022.jpg?tr=w-600"
+            alt="" />
+          <div class="image-text">PREDIKSI CLUB</div>
+        </div>
+        <div class="image-container mt-4">
+          <img src="https://akcdn.detik.net.id/visual/2022/09/28/the-prediksi_43.jpeg?w=650&q=90" alt="" />
+          <div class="image-text">PREDIKSI CLUB</div>
+        </div>
+        <div class="image-container mt-4">
+          <img src="https://rockomotif.com/wp-content/uploads/2023/09/rockomotif-komunitas-motor-prediksi.jpg" alt="" />
+          <div class="image-text">PREDIKSI CLUB</div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="image-container">
+          <img src="https://thumb.viva.id/intipseleb/1265x711/2022/06/29/62bc18042a516-the-prediksi.jpg" alt="" />
+          <div class="image-text">PREDIKSI CLUB</div>
+        </div>
+        <div class="image-container mt-4">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLHVYjPoUqH16YUno5WTgFEFiVc97Xvk0MIQ&usqp=CAU"
+            alt="" />
+          <div class="image-text">PREDIKSI CLUB</div>
+        </div>
+        <div class="image-container mt-4">
+          <img
+            src="https://studio.mrngroup.co/storage/app/media/Prambors/Editorial%202/The%20Prediksi-20220929123623.jpg?tr=w-600"
+            alt="" />
+          <div class="image-text">PREDIKSI CLUB</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container mt-5 mb-5">
+    <div class="row mt-5">
+      <div class="col-md-4 text-center">
+        <div class="card p-4">
+          <h4 class="card-title mb-3">Kemeriahan</h4>
+          <p class="card-text">
+            Bergabunglah dengan kami dan rasakan kemeriahan dari setiap
+            perjalanan kami!
+          </p>
+        </div>
+      </div>
+      <div class="col-md-4 text-center">
+        <div class="card p-4">
+          <h4 class="card-title mb-3">Kebersamaan</h4>
+          <p class="card-text">
+            Di sini, kami bukan hanya anggota klub, tapi juga keluarga yang
+            saling mendukung.
+          </p>
+        </div>
+      </div>
+      <div class="col-md-4 text-center">
+        <div class="card p-4">
+          <h4 class="card-title mb-3">Kreativitas</h4>
+          <p class="card-text">
+            Tunjukkan kreativitas dan gaya mu melalui motor mu!
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row mt-5 mb-5">
+    <div class="col">
+      <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+              aria-expanded="true" aria-controls="collapseOne">
+              Jadwal The Prediksi
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <ul>
+                <li>21 Juni 2024 - Touring ke Bandung</li>
+                <li>5 Juli 2024 - Makan Malam Bersama</li>
+                <li>19 Juli 2024 - Acara Amal</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingTwo">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo"
+              aria-expanded="true" aria-controls="collapseTwo">
+              Sponsor The Prediksi
+            </button>
+          </h2>
+          <div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <ul>
+                <li>PT KAI Indonesia</li>
+                <li>Kratingdaeng</li>
+                <li>PT Harley Davidson</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <footer>
+        <div class="container text-center mt-5 bg-black text-white py-5">
+          <div class="contact-info">
+            <h5>Kontak Kami</h5>
+            <p>Alamat: Jl. Contoh No. 123, Kota, Negara</p>
+            <p>No Telepon: (012) 345-6789</p>
+            <p>Fax: (012) 345-6789</p>
+            <p>Email: info@prediksiclub.com</p>
+          </div>
+        </div>
+      </footer>
+
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
